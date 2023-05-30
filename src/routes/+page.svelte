ec770919c3f2019e8a153b0c5dd63ae9c7c0ee24
jose <arturotoll5@gmail.com>
2023-05-29 23:25:14 -0500

    add name in the home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,7 @@
 </script>
 
 {#if data.session}
-    <h1>Hay un usuario</h1>
-{/if}
-{#if data.session}
+    <h1>{data.session.user.email}</h1>
     <form action="/signOut" method="post">
         <button>Cerrar sesion</button>
     </form>
