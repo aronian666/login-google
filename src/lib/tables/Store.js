823e9d16178857db198c44eff560a0e66ac896b8
Jose Arturo <arturotoll5@gmail.com>
2023-06-20 09:52:56 -0500

    asdf

--- src/lib/tables/Store.js
+++ src/lib/tables/Store.js
@@ -11,10 +11,9 @@ export default class Store extends ActiveRecord {
     static table = "stores"
     async getNewRecord(number = 1, extra) {
         let records = await fetch(this.script + "?threads=" + number).then(r => r.json())
-        console.log(records)
         records = records.map(record => {
             for (const rate of this.rates) {
-                if (rate.type_id === record.type_id && record.value >= rate.min && record.value < rate.max) {
+                if (rate.type_id === record.type_id && record.value >= rate.min && record.value <= rate.max) {
                     record.commission = rate.commission
                     record = { ...record, ...extra }
                     return record
